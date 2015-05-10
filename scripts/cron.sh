56409c8b0d1d63b609b826162668ba0e31703d4b
spacedmonkey <spacedmonkey2@gmail.com>
2015-05-11 00:28:33 +0100

    Changes to cron

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,4 +1,4 @@
-for url in $(bin/wp site list --field=domain)
+for url in $(bin/wp site list --field=domain --allow-root)
 do
     /usr/bin/curl -sS http://localhost/wp-cron.php?doing_wp_cron -H "host: ${url}" 2>&1 &
 done
