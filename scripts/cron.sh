77aa48084e404e344766507a0011eb870139395b
spacedmonkey <spacedmonkey2@gmail.com>
2015-05-11 01:20:12 +0100

    Changed cron for the last time

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,3 +1,5 @@
+export PATH=/usr/local/bin:$PATH
+cd /var/www
 for url in $(bin/wp site list --field=domain --allow-root)
 do
     /usr/bin/curl -sS http://localhost/wp-cron.php?doing_wp_cron -H "host: ${url}" 2>&1 &
