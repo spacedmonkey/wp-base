20069854225c370aded8b449ba6459e5c0139aac
Jonathan Harris <jonathanharris@Jonathans-MacBook.local>
2016-08-24 16:29:02 +0100

    Moved scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,1 @@
 composer update --prefer-dist --no-dev --verbose
-ln -s wp-content /var/www/web/wp-content
-ln -s content /var/www/web/wp-content/uploads
---
+++ scripts/symlinks.sh
@@ -0,0 +1,4 @@
+echo "hi"
+rm -rf web/wp-content;
+ln -s ../wp-content web/wp-content;
+ln -s ../uploads wp-content/uploads;
